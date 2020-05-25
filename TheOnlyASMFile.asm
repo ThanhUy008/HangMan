4d7164233026343be83b42318c9bb4d868971a89
unknown <thanhuy008@gmail.com>
2020-05-25 12:43:27 +0700

    Done read dethi

--- TheOnlyASMFile.asm
+++ TheOnlyASMFile.asm
@@ -4,18 +4,18 @@
 	DrawTake3: .asciiz " _________\n|/       |\n|        O\n|        |\n|\n|\n|\n"
 	DrawTake4: .asciiz " _________\n|/       |\n|        O\n|       /|\n|\n|\n|\n"
 	DrawTake5: .asciiz " _________\n|/       |\n|        O\n|       /|\\ \n|       \n|\n|\n "
-
 	DrawTake6: .asciiz " _________\n|/       |\n|        O\n|       /|\\ \n|       /\n|\n|\n"
 	DrawTake7: .asciiz " _________\n|/       |\n|        O\n|       /|\\ \n|       / \\ \n|\n|\n"
 	TB1: .asciiz "\nEnter n : "
-	
-
+	fin: .asciiz "dethi.txt"
+	onedethi: .space 1024
+	lengthdethi: .word 1
+	savechar: .byte 1
+	endchar: .byte '*'
 .text
 	.globl main
 main:
-
-
-
+	
 	#ket thuc
 	li $v0,10
 	syscall
@@ -89,3 +89,67 @@ _DrawHangMan.End:
 		
 		addi $sp,$sp,32
 		jr $ra
+##Read Dethi
+
+# $a1 have the random number
+#return length of de thi in lengthdethi
+#return array of char dethi in onedethi
+_ReadDeThi:
+		addi $sp,$sp,-32
+		sw $ra,0($sp)
+		sw $t0,4($sp)
+		sw $t1,8 ($sp)
+		sw $t2,12 ($sp)
+		sw $t3,16($sp)
+		sw $t3,20($sp)
+		sw $t4,24($sp)
+		sw $s0,26($sp)
+		sw $s1,32($sp)
+		move $t3,$a1 #save  in t0
+		
+	li   $v0, 13       # system call for open file
+	la   $a0, fin      # input file name
+	li   $a1, 0        # flag for reading
+	li   $a2, 0        # mode is ignored
+	syscall            # open a file 
+	move $s0, $v0      # save the file descriptor 
+
+	li $t4,0
+_ReadDeThi.OuterLoop:
+
+
+	li $t2,0 #count length
+	la $s1,onedethi
+_ReadDeThi.InnerLoop:
+	
+	move $a0,$s0
+	li $v0,14
+	la $a1,savechar
+	li $a2,1
+	syscall
+	#check if end of file
+	beqz $v0,endloop
+	lb $t0,savechar 
+	lb $t1,endchar
+	beq $t0,$t1,_ReadDeThi.EndLoop
+#save char to array:
+	addi $t2,$t2,1 #length++
+	sb $t0,($s1) #a[i++] = $t0 (savechar)
+	addi $s1,$s1,1
+	j _ReadDeThi.InnerLoop
+_ReadDeThi.EndLoop:
+	sw $t2,lengthdethi
+	addi $t4,$t4,1
+	blt $t4,$t3,_ReadDeThi.OuterLoop
+	
+	lw $ra,0($sp)
+	lw $t0,4($sp)
+	lw $t1,8 ($sp)
+	lw $t2,12 ($sp)
+	lw $t3,16($sp)
+	lw $t3,20($sp)
+	lw $t4,24($sp)
+	lw $s0,26($sp)
+	lw $s1,32($sp)
+	addi $sp,$sp,32
+	jr $ra
